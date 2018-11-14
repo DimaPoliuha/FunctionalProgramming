6a4aeaff19681ff11513aadfa5fcc9f234a24829
DmitryPoliuha <dmitry.polyuha@gmail.com>
2018-11-14 23:37:14 +0200

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,4 +1,4 @@
-from math import sin, log
+from math import sin
 
 start = float(input("Input primary x: "))
 finish = float(input("Input final x: "))
@@ -31,15 +31,15 @@ def solution(func, xArray):
 
 print('\n13. Formula: y=sinx/(x+0.5)\n')
 y = solution(lambda x: sin(x) / (x + 0.5), xArray)
-print (y)
+print(y)
 
 print('\n\n18. Formula: y=(x-3)^2/(x+1)^2\n')
 y = solution(lambda x: (x - 3) ** 2 / (x + 1) ** 2, xArray)
-print (y)
+print(y)
 
-print('\n\n18. Formula: y=(x^2+x^3)/lnx\n')
-y = solution(lambda x: (x ** 2 + x ** 3) / log(x), xArray)
-print (y)
+
+
+
 
 
 
@@ -77,47 +77,6 @@ print (y)
 #     print("x: %f" % x)
 #     try:
 #         print("y: %f" % solution(lambda x: (x  2 + x  3) / log(x), x))
-#     except ZeroDivisionError:
-#         print("Divided by zero")
-#     except ValueError:
-#         print("Unacceptable value")
-#     x += step
-
-
-# print('\n13. Formula: y=sinx/(x+0.5)\n')
-#
-# x = start
-#
-# for i in range(n):
-#     print("x: %f" % x)
-#     try:
-#         print("y: %f" % solution(lambda x: sin(x) / (x + 0.5), x))
-#     except ZeroDivisionError:
-#         print("Divided by zero")
-#     except ValueError:
-#         print("Unacceptable value")
-#     x += step
-#
-# print('\n\n18. Formula: y=(x-3)^2/(x+1)^2\n')
-#
-# x = start
-#
-# for i in range(n):
-#     print("x: %f" % x)
-#     try:
-#         print("y: %f" % solution(lambda x: (x - 3) ** 2 / (x + 1) ** 2, x))
-#     except ZeroDivisionError:
-#         print("Divided by zero")
-#     x += step
-#
-# print('\n\n30. Formula: y=(x^2+x^3)/lnx\n')
-#
-# x = start
-#
-# for i in range(n):
-#     print("x: %f" % x)
-#     try:
-#         print("y: %f" % solution(lambda x: (x ** 2 + x ** 3) / log(x), x))
 #     except ZeroDivisionError:
 #         print("Divided by zero")
 #     except ValueError:
