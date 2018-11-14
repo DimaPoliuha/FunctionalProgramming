aa34dec2357d90685de2c10d1d7e86e35c5faa56
DmitryPoliuha <dmitry.polyuha@gmail.com>
2018-11-14 23:37:53 +0200

    dictionary for lab8

---
+++ marks.py
@@ -0,0 +1,13 @@
+marks = {
+    'lab1': 5,
+    'lab2': 4,
+    'module1': 10,
+    'lab3': 2,
+    'lab4': 4,
+    'lab5': 5,
+    'lab6': 3,
+    'module2': 5,
+    'lab7': 5,
+    'lab8': 3,
+}
+
